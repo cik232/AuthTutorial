5038027c8bfb361f551682f99016cc206ab6630f
Azamjon <azamjon@MacBookPro.bbrouter>
2024-10-23 14:31:08 +0500

    Registratsia jarayoni

---
+++ android/app/src/main/kotlin/uz/linux/auth_tutorial/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.linux.auth_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+const String APPWRITE_PROJECT_ID = "643cfa90ef6cdc96a2a9";
+const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
+
+const String APPWRITE_DATABASE_ID = "643d04665fd8051f19be";
+const String COLLECTION_MESSAGES = "643d047649b1cf70e36e";
